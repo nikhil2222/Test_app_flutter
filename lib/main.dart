d1e297dc9c5f5ffdc546e7b20a78c98687dbcfec
nikhil2222 <nikhilkumarnk2209@gmail.com>
2021-06-04 23:48:30 +0530

    box decoration, created box

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,39 @@ class HomePage extends StatelessWidget { //stl
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Awesome app"), title of app
+        title: Text("Awesome app"), //title of app
       ),
-      body: Container(
-      child: Center(child: Text("Hi Flutter")), //center to write in center
-      ),
+      body: Center(
+        child: Container(
+          padding:const EdgeInsets.all(8) ,
+          alignment: Alignment.bottomCenter,
+          width: 100,
+          height: 100,
+
+          decoration: BoxDecoration(
+            color: Colors.teal,
+            borderRadius: BorderRadius.circular(10) ,
+            boxShadow: [BoxShadow(
+              color: Colors.orange,
+              blurRadius: 5,
+              offset: Offset(2.0,5.0)
+
+            )],
+            gradient: LinearGradient(
+
+                colors: [
+              Colors.red,Colors.yellow
+            ])
+          ),
+          child: Text("Hello Nikhil",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),),
+        ),
+      )
     );
   }
 }
